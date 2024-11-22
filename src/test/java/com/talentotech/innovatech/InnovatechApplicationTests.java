af2a5f73c70714487c80e03f1c6b4a9a02402daa
marmo8915 <71729902+marmo8915@users.noreply.github.com>
2024-11-22 01:34:15 +0100

    inicializando proyecto

---
+++ src/test/java/com/talentotech/innovatech/InnovatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentotech.innovatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
